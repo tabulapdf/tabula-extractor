0f4e8249a1163a4ddfe100229a0dc6638d35ebe1
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2013-05-16 16:29:35 -0400

    scale! method and various location helper methods on segment

--- lib/geom/segment.rb
+++ lib/geom/segment.rb
@@ -18,6 +18,14 @@ module Geometry
       self.new(Point.new_by_array(point1_coordinates), 
                Point.new_by_array(point2_coordinates))
     end
+
+    def scale!(scale_factor)
+      self.point1.x = self.point1.x * scale_factor
+      self.point1.y = self.point1.y * scale_factor
+      self.point2.x = self.point2.x * scale_factor
+      self.point2.y = self.point2.y * scale_factor
+    end
+
     def vertical?
       point1.x == point2.x
     end
@@ -40,6 +48,28 @@ module Geometry
 
     def bottommost_endpoint
       ((point1.y <=> point2.y) == -1) ? point1 : point2
+    end
+
+    def top
+      topmost_endpoint.y
+    end
+
+    def bottom
+      bottommost_endpoint.y
+    end
+    def width
+      (left - right).abs
+    end
+    def height
+      (bottom - top).abs
+    end
+
+    def left
+      leftmost_endpoint.x
+    end
+
+    def right
+      rightmost_endpoint.x
     end
     def length      
       Geometry.distance(point1, point2)
