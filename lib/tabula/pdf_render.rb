984cbc416efeea44646e34d4f9aa4016cb81b6aa
Jeremy B. Merrill <jeremy.merrill@propublica.org>
2013-05-21 15:06:47 -0400

    use patched pdfbox

--- lib/tabula/pdf_render.rb
+++ lib/tabula/pdf_render.rb
@@ -1,6 +1,6 @@
 require 'java'
 
-require File.join(File.dirname(__FILE__), '../../target/pdfbox-app-1.8.0.jar')
+require File.join(File.dirname(__FILE__), '../../target/pdfbox-app-1.8.0-plus-r1484277.jar')
 java_import org.apache.pdfbox.pdmodel.PDDocument
 java_import org.apache.pdfbox.pdfviewer.PageDrawer
 java_import java.awt.image.BufferedImage
