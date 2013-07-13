55ec88c4d7e60f2e7b35387ec968fb91c63cf570
Manuel Aristarán <jazzido@jazzido.com>
2013-07-12 23:47:31 -0300

    vectors correctly transformed. now we need to figure out stroking

--- lib/tabula/core_ext.rb
+++ lib/tabula/core_ext.rb
@@ -23,3 +23,17 @@ module Enumerable
   end
 
 end
+
+class java.awt.geom.Line2D::Double
+  def to_json(*args)
+    [self.getX1, self.getY1, self.getX2, self.getY2].to_json(*args)
+  end
+
+  def transform!(affine_transform)
+    newP1, newP2 = java.awt.geom.Point2D::Double.new, java.awt.geom.Point2D::Double.new
+    affine_transform.transform(self.getP1, newP1)
+    affine_transform.transform(self.getP2, newP2)
+    setLine(newP1, newP2)
+    self
+  end
+end
--- lib/tabula/pdf_line_extractor.rb
+++ lib/tabula/pdf_line_extractor.rb
@@ -1,5 +1,6 @@
 require 'java'
 require File.join(File.dirname(__FILE__), '../../target/', Tabula::PDFBOX)
+
 java_import org.apache.pdfbox.util.operator.OperatorProcessor
 java_import org.apache.pdfbox.pdfparser.PDFParser
 java_import org.apache.pdfbox.util.PDFStreamEngine
@@ -7,100 +8,27 @@ java_import org.apache.pdfbox.util.ResourceLoader
 
 java_import java.awt.geom.PathIterator
 java_import java.awt.geom.Point2D
-java_import java.awt.geom.Rectangle2D
 java_import java.awt.geom.GeneralPath
+java_import java.awt.geom.AffineTransform
+
+require_relative './core_ext'
+
 
 class Tabula::Extraction::LineExtractor < org.apache.pdfbox.util.PDFStreamEngine
 
-  # TODO: reuse Tabula::ZoneEntity
-  class GenericSegment < Struct.new(:x1, :y1, :x2, :y2)
-    alias_method :lower_left_x, :x1
-    alias_method :lower_left_y, :y1
-    alias_method :upper_right_x, :x2
-    alias_method :upper_right_y, :y2
-
-    def initialize(*args)
-      super(*args)
-      # correct negative dimensions
-      if self.x1 > self.x2
-        temp = self.x1
-        self.x1 = self.x2
-        self.x2 = temp
-      end
-
-      if self.y1 > self.y2
-        temp = self.y1
-        self.y1 = self.y2
-        self.y2 = temp
-      end
-    end
-
-    def to_xml
-      attrs = [:x1, :x2, :y1, :y2].map { |k|
-        "#{k.to_s}=\"#{self.send(k)}\""
-      }.join(' ')
-      "<#{self.class} #{attrs} />"
-    end
-
-    def rotate(*args)
-      if args.size == 3
-
-        pointX, pointY, amount = args
-
-        px1 = self.x1 - pointX; px2 = self.x2 - pointX
-        py1 = self.y1 - pointY; py2 = self.y2 - pointY
-
-        if (amount == 90 || amount == -270)
-          self.x1 = pointX - py2; self.x2 = pointX - py1;
-          self.y1 = pointY + px1; y2 = pointY + px2;
-        elsif (amount == 270 || amount == -90)
-          self.x1 = pointX + py1; self.x2 = pointX + py2;
-          self.y1 = pointY - px2; self.y2 = pointY - px1;
-        end
-      elsif args.size == 1
-        page = args.first
-        mediaBox = page.getMediaBox
-        if !page.getRotation.nil?
-          rotate(mediaBox.getLowerLeftX, mediaBox.getLowerLeftY, page.getRotation);
-          if (page.getRotation == 90 || page.getRotation == -270)
-            self.x1 = x1 + mediaBox.getHeight()
-            self.x2 = x2 + mediaBox.getHeight()
-          elsif (page.getRotation() == 270 || page.getRotation() == -90)
-            self.y1 = y1 + mediaBox.getWidth()
-            self.y2 = y2 + mediaBox.getWidth()
-          end
-        end
-      end
-    end
-  end
-
-  class LineSegment < GenericSegment
-  end
-
-  class RectSegment < GenericSegment
-
-    ##
-    # returns an array of the component lines of this rectangle
-    def to_lines
-      raise "TODO: Not Implemented!"
-    end
-  end
-
-
-  attr_accessor :lineSubPaths, :linePath, :lineList, :rectList, :currentLines, :currentRects, :linesToAdd,  :rectsToAdd, :strokingColor
-  attr_accessor :newPath, :pathContainsCurve, :pathBeginSet, :pathClosed
   attr_accessor :currentX, :currentY
+  attr_accessor :rulings
   field_accessor :page
-
 
   class LineToOperator < org.apache.pdfbox.util.operator.OperatorProcessor
     def process(operator, arguments)
       drawer = self.context
       x, y = arguments[0], arguments[1]
       ppos = drawer.TransformedPoint(x.doubleValue, y.doubleValue)
-      puts "LINE TO #{ppos.getX}, #{ppos.getY}"
-      drawer.linePath.lineTo(ppos.getX, ppos.getY)
-#      drawer.simple_line_to!(x.doubleValue, y.doubleValue)
+
+      drawer.addRuling java.awt.geom.Line2D::Double.new(drawer.currentX, drawer.currentY, ppos.getX, ppos.getY)
+
+      drawer.currentX, drawer.currentY = ppos.getX, ppos.getY
     end
   end
 
@@ -110,23 +38,17 @@ class Tabula::Extraction::LineExtractor < org.apache.pdfbox.util.PDFStreamEngine
       drawer = self.context
       x, y = arguments[0], arguments[1]
 
-      drawer.lineSubPaths << drawer.linePath
-      newPath = java.awt.geom.GeneralPath.new
       ppos = drawer.TransformedPoint(x.doubleValue, y.doubleValue)
 
-      puts "MOVE TO #{ppos.getX}, #{ppos.getY}"
-
-      newPath.moveTo(ppos.getX, ppos.getY)
-      drawer.linePath = newPath
-      drawer.simple_move_to!(x.doubleValue, y.doubleValue)
+      drawer.currentX, drawer.currentY = ppos.getX, ppos.getY
     end
   end
 
   class AppendRectangleToPathOperator < org.apache.pdfbox.util.operator.OperatorProcessor
     def process(operator, arguments)
       drawer = self.context
-      x, y, w, h = arguments.to_array
-      finalX, finalY, finalW, finalH = x.doubleValue, y.doubleValue, w.doubleValue, h.doubleValue
+      finalX, finalY, finalW, finalH = arguments.to_array.map(&:doubleValue)
+
       ppos = drawer.TransformedPoint(finalX, finalY)
       psize = drawer.ScaledPoint(finalW, finalH)
 
@@ -135,114 +57,106 @@ class Tabula::Extraction::LineExtractor < org.apache.pdfbox.util.PDFStreamEngine
         finalY = 0
       end
 
-      rect = java.awt.geom.Rectangle2D::Double.new(ppos.getX, finalY, psize.getX, psize.getY)
-      drawer.linePath.reset
-      drawer.linePath.append(rect, false)
+      # add every edge of the rectangle to drawer.rulings
+      drawer.addRuling java.awt.geom.Line2D::Double.new(ppos.getX, finalY, ppos.getX + psize.getX, finalY)
+      drawer.addRuling java.awt.geom.Line2D::Double.new(ppos.getX, finalY, ppos.getX, finalY + psize.getY)
+      drawer.addRuling java.awt.geom.Line2D::Double.new(ppos.getX+psize.getX, finalY, ppos.getX + psize.getX, finalY + psize.getY)
+      drawer.addRuling java.awt.geom.Line2D::Double.new(ppos.getX, finalY+psize.getY, ppos.getX + psize.getX, finalY + psize.getY)
 
-      drawer.new_path!
-      drawer.simple_add_rect!(ppos.get_x, finalY, psize.getX, psize.getY)
     end
   end
+
+  DETECT_LINES_DEFAULTS = {}
+
+  def self.lines_in_pdf_page(pdf_path, page_number, options={})
+    options = DETECT_LINES_DEFAULTS.merge(options)
+
+    pdf_file = ::Tabula::Extraction.openPDF(pdf_path)
+    page = pdf_file.getDocumentCatalog.getAllPages[page_number]
+    le = self.new
+    le.processStream(page, page.findResources, page.getContents.getStream)
+
+    return le.rulings
+  end
+
+  OPERATOR_PROCESSORS = {
+    'm' => MoveToOperator.new,
+    're' => AppendRectangleToPathOperator.new,
+    'l' => LineToOperator.new,
+    'BT' => org.apache.pdfbox.util.operator.BeginText.new,
+    'cm' => org.apache.pdfbox.util.operator.Concatenate.new,
+    'CS' => org.apache.pdfbox.util.operator.SetStrokingColorSpace.new,
+    'cs' => org.apache.pdfbox.util.operator.SetNonStrokingColorSpace.new,
+    'ET' => org.apache.pdfbox.util.operator.EndText.new,
+    'G' => org.apache.pdfbox.util.operator.SetStrokingGrayColor.new,
+    'g' => org.apache.pdfbox.util.operator.SetNonStrokingGrayColor.new,
+    'gs' => org.apache.pdfbox.util.operator.SetGraphicsStateParameters.new,
+    'K' => org.apache.pdfbox.util.operator.SetStrokingCMYKColor.new,
+    'k' => org.apache.pdfbox.util.operator.SetNonStrokingCMYKColor.new,
+    'q' => org.apache.pdfbox.util.operator.GSave.new,
+    'Q' => org.apache.pdfbox.util.operator.GRestore.new,
+    'RG' => org.apache.pdfbox.util.operator.SetStrokingRGBColor.new,
+    'rg' => org.apache.pdfbox.util.operator.SetNonStrokingRGBColor.new,
+    's' => org.apache.pdfbox.util.operator.CloseAndStrokePath.new,
+    'SC' => org.apache.pdfbox.util.operator.SetStrokingColor.new,
+    'sc' => org.apache.pdfbox.util.operator.SetNonStrokingColor.new,
+    'SCN' => org.apache.pdfbox.util.operator.SetStrokingColor.new,
+    'scn' => org.apache.pdfbox.util.operator.SetNonStrokingColor.new,
+    'T*' => org.apache.pdfbox.util.operator.NextLine.new,
+    'Tc' => org.apache.pdfbox.util.operator.SetCharSpacing.new,
+    'Td' => org.apache.pdfbox.util.operator.MoveText.new,
+    'TD' => org.apache.pdfbox.util.operator.MoveTextSetLeading.new,
+    'Tf' => org.apache.pdfbox.util.operator.SetTextFont.new,
+    'Tj' => org.apache.pdfbox.util.operator.ShowText.new,
+    'TJ' => org.apache.pdfbox.util.operator.ShowTextGlyph.new,
+    'TL' => org.apache.pdfbox.util.operator.SetTextLeading.new,
+    'Tm' => org.apache.pdfbox.util.operator.SetMatrix.new,
+    'Tr' => org.apache.pdfbox.util.operator.SetTextRenderingMode.new,
+    'Ts' => org.apache.pdfbox.util.operator.SetTextRise.new,
+    'Tw' => org.apache.pdfbox.util.operator.SetWordSpacing.new,
+    'Tz' => org.apache.pdfbox.util.operator.SetHorizontalTextScaling.new,
+    "\'" => org.apache.pdfbox.util.operator.MoveAndShow.new,
+    '\"' => org.apache.pdfbox.util.operator.SetMoveAndShow.new,
+  }
+
 
   def initialize
     super
     self.clear!
-    registerOperatorProcessor('m', MoveToOperator.new)
-    registerOperatorProcessor('re', AppendRectangleToPathOperator.new)
-    registerOperatorProcessor('l', LineToOperator.new)
-    registerOperatorProcessor('BT', org.apache.pdfbox.util.operator.BeginText.new)
-    registerOperatorProcessor('cm', org.apache.pdfbox.util.operator.Concatenate.new)
-    registerOperatorProcessor('CS', org.apache.pdfbox.util.operator.SetStrokingColorSpace.new)
-    registerOperatorProcessor('cs', org.apache.pdfbox.util.operator.SetNonStrokingColorSpace.new)
-    registerOperatorProcessor('ET', org.apache.pdfbox.util.operator.EndText.new)
-    registerOperatorProcessor('G', org.apache.pdfbox.util.operator.SetStrokingGrayColor.new)
-    registerOperatorProcessor('g', org.apache.pdfbox.util.operator.SetNonStrokingGrayColor.new)
-    registerOperatorProcessor('gs', org.apache.pdfbox.util.operator.SetGraphicsStateParameters.new)
-    registerOperatorProcessor('K', org.apache.pdfbox.util.operator.SetStrokingCMYKColor.new)
-    registerOperatorProcessor('k', org.apache.pdfbox.util.operator.SetNonStrokingCMYKColor.new)
-    registerOperatorProcessor('q', org.apache.pdfbox.util.operator.GSave.new)
-    registerOperatorProcessor('Q', org.apache.pdfbox.util.operator.GRestore.new)
-    registerOperatorProcessor('RG', org.apache.pdfbox.util.operator.SetStrokingRGBColor.new)
-    registerOperatorProcessor('rg', org.apache.pdfbox.util.operator.SetNonStrokingRGBColor.new)
-    registerOperatorProcessor('s', org.apache.pdfbox.util.operator.CloseAndStrokePath.new)
-    registerOperatorProcessor('SC', org.apache.pdfbox.util.operator.SetStrokingColor.new)
-    registerOperatorProcessor('sc', org.apache.pdfbox.util.operator.SetNonStrokingColor.new)
-    registerOperatorProcessor('SCN', org.apache.pdfbox.util.operator.SetStrokingColor.new)
-    registerOperatorProcessor('scn', org.apache.pdfbox.util.operator.SetNonStrokingColor.new)
-    registerOperatorProcessor('T*', org.apache.pdfbox.util.operator.NextLine.new)
-    registerOperatorProcessor('Tc', org.apache.pdfbox.util.operator.SetCharSpacing.new)
-    registerOperatorProcessor('Td', org.apache.pdfbox.util.operator.MoveText.new)
-    registerOperatorProcessor('TD', org.apache.pdfbox.util.operator.MoveTextSetLeading.new)
-    registerOperatorProcessor('Tf', org.apache.pdfbox.util.operator.SetTextFont.new)
-    registerOperatorProcessor('Tj', org.apache.pdfbox.util.operator.ShowText.new)
-    registerOperatorProcessor('TJ', org.apache.pdfbox.util.operator.ShowTextGlyph.new)
-    registerOperatorProcessor('TL', org.apache.pdfbox.util.operator.SetTextLeading.new)
-    registerOperatorProcessor('Tm', org.apache.pdfbox.util.operator.SetMatrix.new)
-    registerOperatorProcessor('Tr', org.apache.pdfbox.util.operator.SetTextRenderingMode.new)
-    registerOperatorProcessor('Ts', org.apache.pdfbox.util.operator.SetTextRise.new)
-    registerOperatorProcessor('Tw', org.apache.pdfbox.util.operator.SetWordSpacing.new)
-    registerOperatorProcessor('Tz', org.apache.pdfbox.util.operator.SetHorizontalTextScaling.new)
-    registerOperatorProcessor("\'", org.apache.pdfbox.util.operator.MoveAndShow.new)
-    registerOperatorProcessor('\"', org.apache.pdfbox.util.operator.SetMoveAndShow.new)
+    OPERATOR_PROCESSORS.each { |k,v| registerOperatorProcessor(k, v) }
   end
 
   def clear!
-    self.lineSubPaths = []
-    self.linePath = java.awt.geom.GeneralPath.new
-    self.lineList = []
-    self.rectList = []
-    self.currentLines = []
-    self.currentRects = []
-    self.linesToAdd = []
-    self.rectsToAdd = []
-    self.newPath = false
+    self.rulings = []
     self.currentX = -1; self.currentY = -1
+    @pageSize = nil
   end
 
-  def new_path!
-    self.linesToAdd += currentLines
-    self.rectsToAdd += currentRects
-    self.newPath = true
-    self.pathContainsCurve = false
-    self.pathBeginSet = false
-    self.pathClosed = false
-    self.currentLines = []
-    self.currentRects = []
-  end
+  def addRuling(ruling)
+    if !page.getRotation.nil? and [90, -270, -90, 270].include?(page.getRotation)
 
-  def simple_move_to!(x, y)
-    ppos = self.TransformedPoint(x, y)
-    self.currentX, self.currentY = ppos.getX, ppos.getY
-  end
+      mb = page.getMediaBox
+      affine_transform = AffineTransform.getQuadrantRotateInstance(self.page.getRotation / 90, mb.getLowerLeftX, mb.getLowerLeftY)
 
-  def simple_line_to!(x, y)
-    # TODO include color
-    # comp = strokingColor.getRGBColorComponents(nil)
-    pto = self.TransformedPoint(x, y)
-
-    newLine = LineSegment.new(currentX, currentY, pto.getX, pto.getY)
-    newLine.rotate(self.page)
-    self.linesToAdd << newLine
-    self.currentX = pto.getX; self.currentY = pto.getY
-#    puts "line from: (#{newLine.x1}, #{newLine.y1}) -> (#{newLine.x2}, #{newLine.y2})"
-  end
-
-  def simple_add_rect!(x, y, w, h)
-    # TODO include color
-    # comp = getStrokingColor.getRGBColorComponents(nil)
-    newRect = RectSegment.new(x, x+w, y, y+h)
-    newRect.rotate(self.page)
-    self.rectsToAdd << newRect
-    self.currentX = x
-    self.currentY = y
-    #puts "rectangle (#{newRect.x1}, #{newRect.y1}) -> (#{newRect.x2}, #{newRect.y2})"
-#    puts newRect.to_xml
+      trans = if page.getRotation == 90 || page.getRotation == -270
+                AffineTransform.getTranslateInstance(mb.getHeight, 0)
+              else
+                AffineTransform.getTranslateInstance(0, mb.getWidth)
+              end
+      scale = AffineTransform.getScaleInstance(mb.getWidth / mb.getHeight,
+                                               mb.getWidth / mb.getHeight)
+      trans.concatenate(affine_transform)
+      scale.concatenate(trans)
+      self.rulings << ruling.transform!(scale)
+    else
+      self.rulings << ruling
+    end
   end
 
   ##
   # get current page size
   def pageSize
-    self.page.findMediaBox.createDimension
+    @pageSize ||= self.page.findMediaBox.createDimension
   end
 
   ##
@@ -279,11 +193,10 @@ class Tabula::Extraction::LineExtractor < org.apache.pdfbox.util.PDFStreamEngine
 
   def TransformedPoint(x, y)
     position = [x,y].to_java(:double)
-    self.getGraphicsState.getCurrentTransformationMatrix.createAffineTransform.transform(position, 0, position, 0, 1)
+    at = self.getGraphicsState.getCurrentTransformationMatrix.createAffineTransform
+    at.transform(position, 0, position, 0, 1)
     position[1] = fixY(position[1])
     java.awt.geom.Point2D::Double.new(position[0], position[1])
   end
-
-
 
 end
