6b9003060ef50f7a745ea5c972cadb59b340647a
Manuel Aristarán <jazzido@jazzido.com>
2013-11-20 18:50:23 -0300

    inspect for TextElement

--- lib/tabula/entities.rb
+++ lib/tabula/entities.rb
@@ -126,6 +126,10 @@ module Tabula
       hash
     end
 
+    def inspect
+      "#<TextElement: #{self.top.round(2)},#{self.left.round(2)},#{self.bottom.round(2)},#{right.round(2)} '#{self.text}'"
+    end
+
     def ==(other)
       self.text.strip == other.text.strip
     end
