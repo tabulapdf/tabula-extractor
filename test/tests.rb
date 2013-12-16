501141dd164aa7d479a1aff360e5e69fa6c03ab4
Jeremy B. Merrill <jeremybmerrill@gmail.com>
2013-12-15 21:03:02 -0500

    revise a test to be clearer

--- test/tests.rb
+++ test/tests.rb
@@ -489,17 +489,12 @@ class TestExtractor < Minitest::Test
 
   def test_cope_with_a_tableless_page
     pdf_file_path = "./test/data/Auth1.pdf"
-    expected = ""
-
-    actual = ""
-
-    Tabula::Extraction::SpreadsheetExtractor.new(pdf_file_path, :all).extract(
+    
+    spreadsheets = Tabula::Extraction::SpreadsheetExtractor.new(pdf_file_path, :all).extract(
         :line_color_filter => lambda{|components| components.all?{|c| c < 0.2}} 
-      ).each do |pdf_page, spreadsheet|
-      puts pdf_page.ruling_lines
-      actual << spreadsheet.to_tsv
-    end
-    assert_equal expected, actual
+      ).to_a
+
+    assert_equal 0, spreadsheets.size
   end
 
 end
