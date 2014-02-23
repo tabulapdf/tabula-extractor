4ba27af5d8564c60154029043bfe25c2fa6f2045
Jeremy B. Merrill <jeremybmerrill@gmail.com>
2014-02-23 13:09:38 -0500

    forgot a file, and cleanup

--- lib/tabula/entities/has_cells.rb
+++ lib/tabula/entities/has_cells.rb
@@ -26,7 +26,6 @@ module Tabula
       ((columns_defined_by_lines.to_f / columns_defined_without_lines) + (rows_defined_by_lines.to_f / rows_defined_without_lines)) / 2
     end
 
-
     # finds cells from the ruling lines on the page.
     # implements Nurminen thesis algorithm cf. https://github.com/jazzido/tabula-extractor/issues/16
     # subclasses must define cells, vertical_ruling_lines, horizontal_ruling_lines accessors
@@ -90,66 +89,6 @@ module Tabula
       end
       self.cells = cellsFound
       cellsFound
-    end
-
-    #############################
-    # Chapter 2, Spanning Cells #
-    #############################
-    #if c is a "spanning cell", that is
-    #              if there are N>0 vertical lines strictly between this cell's left and right
-    #insert N placeholder cells after it with zero size (but same top)
-
-    # subclasses must define cells, vertical_ruling_lines, horizontal_ruling_lines accessors
-    def add_spanning_cells!
-      #rounding: because Cell.new_from_points, using in #find_cells above, has
-      # a float precision error where, for instance, a cell whose x2 coord is
-      # supposed to be 160.137451171875 comes out as 160.13745498657227 because
-      # of minus. :(
-      vertical_uniq_locs = vertical_ruling_lines.map{|l| l.left.round(5)}.uniq    #already sorted
-      horizontal_uniq_locs = horizontal_ruling_lines.map{|l| l.top.round(5)}.uniq #already sorted
-
-      cells.each do |c|
-        vertical_rulings_spanned_over = vertical_uniq_locs.select{|l| l > c.left.round(5) && l < c.right.round(5) }
-        horizontal_rulings_spanned_over = horizontal_uniq_locs.select{|t| t > c.top.round(5) && t < c.bottom.round(5) }
-
-        unless vertical_rulings_spanned_over.empty?
-          c.spanning = true
-          vertical_rulings_spanned_over.each do |spanned_over_line_loc|
-            placeholder = Cell.new(c.top, spanned_over_line_loc, 0, c.height)
-            placeholder.placeholder = true
-            cells << placeholder
-          end
-        end
-        unless horizontal_rulings_spanned_over.empty?
-          c.spanning = true
-          horizontal_rulings_spanned_over.each do |spanned_over_line_loc|
-            placeholder = Cell.new(spanned_over_line_loc, c.left, c.width, 0)
-            placeholder.placeholder = true
-            cells << placeholder
-          end
-        end
-
-        #if there's a spanning cell that's spans over both rows and columns, then it has "double placeholder" cells
-        # e.g. -------------------
-        #      | C |  C |  C | C |         (this is some pretty sweet ASCII art, eh?)
-        #      |-----------------|
-        #      | C |  C |  C | C |
-        #      |-----------------|
-        #      | C | SC    P | C |   where MC is the "spanning cell" that holds all the text within its bounds
-        #      |----    +    ----|         P is a "placeholder" cell with either zero width or zero height
-        #      | C | P    DP | C |         DP is a "double placeholder" cell with zero width and zero height
-        #      |----    +    ----|         C is an ordinary cell.
-        #      | C | P    DP | C |
-        #      |-----------------|
-
-        unless (double_placeholders = vertical_rulings_spanned_over.product(horizontal_rulings_spanned_over)).empty?
-          double_placeholders.each do |vert_spanned_over, horiz_spanned_over|
-            placeholder = Cell.new(horiz_spanned_over, vert_spanned_over, 0, 0)
-            placeholder.placeholder = true
-            cells << placeholder
-          end
-        end
-      end
     end
 
     #TODO:
--- lib/tabula/entities/spreadsheet.rb
+++ lib/tabula/entities/spreadsheet.rb
@@ -6,7 +6,6 @@ module Tabula
 
   class Spreadsheet < ZoneEntity
     include Tabula::Tabular
-    include Tabula::HasCells
     attr_accessor :cells, :vertical_ruling_lines, :horizontal_ruling_lines, :cells_resolved
     attr_reader :extraction_method, :page
 
@@ -70,6 +69,64 @@ module Tabula
       lefts = cells.map(&:left).uniq.sort
       lefts.map do |left|
         cells.select{|c| c.left == left }.sort_by(&:top)
+      end
+    end
+
+    #######################################################
+    # Chapter 2 of Spreadsheet extraction, Spanning Cells #
+    #######################################################
+    #if c is a "spanning cell", that is
+    #              if there are N>0 vertical lines strictly between this cell's left and right
+    #insert N placeholder cells after it with zero size (but same top)
+    def add_spanning_cells!
+      #rounding: because Cell.new_from_points, using in #find_cells above, has
+      # a float precision error where, for instance, a cell whose x2 coord is
+      # supposed to be 160.137451171875 comes out as 160.13745498657227 because
+      # of minus. :(
+      vertical_uniq_locs = vertical_ruling_lines.map{|l| l.left.round(5)}.uniq    #already sorted
+      horizontal_uniq_locs = horizontal_ruling_lines.map{|l| l.top.round(5)}.uniq #already sorted
+
+      cells.each do |c|
+        vertical_rulings_spanned_over = vertical_uniq_locs.select{|l| l > c.left.round(5) && l < c.right.round(5) }
+        horizontal_rulings_spanned_over = horizontal_uniq_locs.select{|t| t > c.top.round(5) && t < c.bottom.round(5) }
+
+        unless vertical_rulings_spanned_over.empty?
+          c.spanning = true
+          vertical_rulings_spanned_over.each do |spanned_over_line_loc|
+            placeholder = Cell.new(c.top, spanned_over_line_loc, 0, c.height)
+            placeholder.placeholder = true
+            cells << placeholder
+          end
+        end
+        unless horizontal_rulings_spanned_over.empty?
+          c.spanning = true
+          horizontal_rulings_spanned_over.each do |spanned_over_line_loc|
+            placeholder = Cell.new(spanned_over_line_loc, c.left, c.width, 0)
+            placeholder.placeholder = true
+            cells << placeholder
+          end
+        end
+
+        #if there's a spanning cell that's spans over both rows and columns, then it has "double placeholder" cells
+        # e.g. -------------------
+        #      | C |  C |  C | C |         (this is some pretty sweet ASCII art, eh?)
+        #      |-----------------|
+        #      | C |  C |  C | C |
+        #      |-----------------|
+        #      | C | SC    P | C |   where MC is the "spanning cell" that holds all the text within its bounds
+        #      |----    +    ----|         P is a "placeholder" cell with either zero width or zero height
+        #      | C | P    DP | C |         DP is a "double placeholder" cell with zero width and zero height
+        #      |----    +    ----|         C is an ordinary cell.
+        #      | C | P    DP | C |
+        #      |-----------------|
+
+        unless (double_placeholders = vertical_rulings_spanned_over.product(horizontal_rulings_spanned_over)).empty?
+          double_placeholders.each do |vert_spanned_over, horiz_spanned_over|
+            placeholder = Cell.new(horiz_spanned_over, vert_spanned_over, 0, 0)
+            placeholder.placeholder = true
+            cells << placeholder
+          end
+        end
       end
     end
 
