2809f54a0cb1c2f2928a9fe4a5116fa9942704ef
Manuel Aristarán <jazzido@jazzido.com>
2014-05-01 20:21:04 -0500

    more porting to java, more milisecs shaved

--- ext/tabula/src/org/nerdpower/tabula/Ruling.java
+++ ext/tabula/src/org/nerdpower/tabula/Ruling.java
@@ -2,12 +2,24 @@ package org.nerdpower.tabula;
 
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 @SuppressWarnings("serial")
 public class Ruling extends Line2D.Float {
+    
+    private static int PERPENDICULAR_PIXEL_EXPAND_AMOUNT = 2;
+    private static int COLINEAR_OR_PARALLEL_PIXEL_EXPAND_AMOUNT = 1;
 
     public Ruling(float top, float left, float width, float height) {
         super(left, top, left+width, top+height);
+    }
+    
+    public Ruling(Point2D p1, Point2D p2) {
+        super(p1, p2);
     }
 
     public boolean vertical() {
@@ -22,6 +34,70 @@ public class Ruling extends Line2D.Float {
         return !(this.vertical() || this.horizontal());
     }
     
+    // attributes that make sense only for non-oblique lines
+    // these are used to have a single collapse method (in page, currently)
+    
+    public float getPosition() {
+        if (this.oblique()) {
+            throw new UnsupportedOperationException();
+        }
+        return this.vertical() ? this.getLeft() : this.getTop();
+    }
+    
+    public void setPosition(float v) {
+        if (this.oblique()) {
+            throw new UnsupportedOperationException();
+        }
+        if (this.vertical()) {
+            this.setLeft(v);
+            this.setRight(v);
+        }
+        else {
+            this.setTop(v);
+            this.setBottom(v);
+        }
+    }
+    
+    public float getStart() {
+        if (this.oblique()) {
+            throw new UnsupportedOperationException();
+        }
+        return this.vertical() ? this.getTop() : this.getLeft();
+    }
+    
+    public void setStart(float v) {
+        if (this.oblique()) {
+            throw new UnsupportedOperationException();
+        }
+        if (this.vertical()) {
+            this.setTop(v);
+        }
+        else {
+            this.setLeft(v);
+        }
+    }
+    
+    public float getEnd() {
+        if (this.oblique()) {
+            throw new UnsupportedOperationException();
+        }
+        return this.vertical() ? this.getBottom() : this.getRight();
+    }
+    
+    public void setEnd(float v) {
+        if (this.oblique()) {
+            throw new UnsupportedOperationException();
+        }
+        if (this.vertical()) {
+            this.setBottom(v);
+        }
+        else {
+            this.setRight(v);
+        }
+    }
+    
+    // -----
+        
     public boolean perpendicularTo(Ruling other) {
         return this.vertical() == other.horizontal();
     }
@@ -33,10 +109,144 @@ public class Ruling extends Line2D.Float {
                 && point.getY() <= this.getY2();
     }
     
+    // if the lines we're comparing are colinear or parallel, we expand them by a only 1 pixel,
+    // because the expansions are additive
+    // (e.g. two vertical lines, at x = 100, with one having y2 of 98 and the other having y1 of 102 would
+    // erroneously be said to nearlyIntersect if they were each expanded by 2 (since they'd both terminate at 100).
+    // The COLINEAR_OR_PARALLEL_PIXEL_EXPAND_AMOUNT is only 1 so the total expansion is 2.
+    // A total expansion amount of 2 is empirically verified to work sometimes. It's not a magic number from any
+    // source other than a little bit of experience.)
+    public boolean nearlyIntersects(Ruling another) {
+        if (this.intersectsLine(another)) {
+            return true;
+        }
+        
+        boolean rv = false;
+        
+        if (this.perpendicularTo(another)) {
+            rv = this.expand(PERPENDICULAR_PIXEL_EXPAND_AMOUNT).intersectsLine(another);
+        }
+        else {
+            rv = this.expand(COLINEAR_OR_PARALLEL_PIXEL_EXPAND_AMOUNT)
+                    .intersectsLine(another.expand(COLINEAR_OR_PARALLEL_PIXEL_EXPAND_AMOUNT));
+        }
+        
+        return rv;
+    }
+    
     public double length() {
         return Math.sqrt(Math.pow(this.getX1() - this.getX2(), 2) + Math.pow(this.getY1() - this.getY2(), 2));
     }
     
+    public Ruling intersect(Rectangle2D clip) {
+        Line2D.Float clipee = (Line2D.Float) this.clone();
+        boolean clipped = new CohenSutherlandClipping(clip).clip(clipee);
+
+        if (clipped) {
+            return new Ruling(clipee.getP1(), clipee.getP2());
+        }
+        else {
+            return this;
+        }
+    }
     
+    public Ruling expand(float amount) {
+        Ruling r = (Ruling) this.clone();
+        r.setStart(this.getStart() - amount);
+        r.setEnd(this.getEnd() + amount);
+        return r;
+    }
     
+    public Point2D intersectionPoint(Ruling other) {
+        Ruling this_l = this.expand(PERPENDICULAR_PIXEL_EXPAND_AMOUNT);
+        Ruling other_l = other.expand(PERPENDICULAR_PIXEL_EXPAND_AMOUNT);
+        Ruling horizontal, vertical;
+        
+        if (!this_l.intersectsLine(other_l)) {
+            return null;
+        }
+        
+        if (this_l.horizontal() && other_l.vertical()) {
+            horizontal = this_l; vertical = other_l;
+        }
+        else if (this_l.vertical() && other_l.horizontal()) {
+            vertical = this_l; horizontal = other_l;
+        }
+        else {
+            throw new IllegalArgumentException("lines must be orthogonal, vertical and horizontal");
+        }
+        return new Point2D.Float(vertical.getLeft(), horizontal.getTop());        
+    }
+    
+    public float getTop() {
+        return (float) this.getY1();
+    }
+    
+    public void setTop(float v) {
+        setLine(this.getLeft(), v, this.getRight(), this.getBottom());
+    }
+    
+    public float getLeft() {
+        return (float) this.getX1();
+    }
+    
+    public void setLeft(float v) {
+        setLine(v, this.getTop(), this.getRight(), this.getBottom());
+    }
+    
+    public float getBottom() {
+        return (float) this.getY2();
+    }
+    
+    public void setBottom(float v) {
+        setLine(this.getLeft(), this.getTop(), this.getRight(), v);
+    }
+
+    public float getRight() {
+        return (float) this.getX2();
+    }
+    
+    public void setRight(float v) {
+        setLine(this.getLeft(), this.getTop(), v, this.getBottom());
+    }
+    
+    public static List<Ruling> cropRulingsToArea(List<Ruling> rulings, Rectangle2D area) {
+        ArrayList<Ruling> rv = new ArrayList<Ruling>();
+        for (Ruling r : rulings) {
+            if (r.intersects(area)) {
+                rv.add(r.intersect(area));
+            }
+        }
+        return rv;
+    }
+    
+    public static List<Ruling> collapseOrientedRulings(List<Ruling> lines) {
+        ArrayList<Ruling> rv = new ArrayList<Ruling>();
+        if (lines.size() == 0) {
+            return rv;
+        }
+        Collections.sort(lines, new Comparator<Ruling>() {
+            @Override
+            public int compare(Ruling a, Ruling b) {
+                return (int) (a.getPosition() != b.getPosition() ? a.getPosition() - b.getPosition() : a.getStart() - b.getStart());
+            }
+        });
+        
+        rv.add(lines.remove(0));
+        for (Ruling next_line : lines) {
+            Ruling last = rv.get(rv.size() - 1);
+            // if current line colinear with next, and are "close enough": expand current line
+            if (next_line.getPosition() == last.getPosition() && last.nearlyIntersects(next_line)) {
+                last.setStart(next_line.getStart() < last.getStart() ? next_line.getStart() : last.getStart());
+                last.setEnd(next_line.getEnd() < last.getEnd() ? last.getEnd() : next_line.getEnd());
+            }
+            else if (next_line.length() == 0) {
+                continue;
+            }
+            else {
+                rv.add(next_line);
+            }
+        }
+        return rv;
+    }
 }
--- lib/tabula/entities/ruling.rb
+++ lib/tabula/entities/ruling.rb
@@ -1,27 +1,6 @@
 java_import org.nerdpower.tabula.Ruling
 
 class Ruling
-  alias :top :getY1
-  alias :left :getX1
-  alias :bottom :getY2
-  alias :right :getX2
-
-  def top=(v)
-    self.java_send :setLine, [Java::float, Java::float, Java::float, Java::float,], left, v, right, bottom
-  end
-
-  def left=(v)
-    self.java_send :setLine, [Java::float, Java::float, Java::float, Java::float,], v, top, right, bottom
-  end
-
-  def bottom=(v)
-    self.java_send :setLine, [Java::float, Java::float, Java::float, Java::float,], left, top, right, v
-  end
-
-  def right=(v)
-    self.java_send :setLine, [Java::float, Java::float, Java::float, Java::float,], left, top, v, bottom
-  end
-
   def width
     right - left
   end
@@ -30,51 +9,6 @@ class Ruling
     bottom - top
   end
 
-  # attributes that make sense only for non-oblique lines
-  # these are used to have a single collapse method (in page, currently)
-
-  ##
-  # `x` (left) coordinate if line vertical, `y` (top) if horizontal
-  def position
-    raise NoMethodError, "Oblique line #{self.inspect} has no #position method." if oblique?
-    vertical? ? left : top
-  end
-  def start
-    raise NoMethodError, "Oblique line #{self.inspect} has no #start method." if oblique?
-    vertical? ? top : left
-  end
-  def end
-    raise NoMethodError, "Oblique line #{self.inspect} has no #end method." if oblique?
-    vertical? ? bottom : right
-  end
-  def position=(coord)
-    raise NoMethodError, "Oblique line #{self.inspect} has no #position= method." if oblique?
-    if vertical?
-      self.left = coord
-      self.right = coord
-    else
-      self.top = coord
-      self.bottom = coord
-    end
-  end
-  def start=(coord)
-    raise NoMethodError, "Oblique line #{self.inspect} has no #start= method." if oblique?
-    if vertical?
-      self.top = coord
-    else
-      self.left = coord
-    end
-  end
-  def end=(coord)
-    raise NoMethodError, "Oblique line #{self.inspect} has no #end= method." if oblique?
-    if vertical?
-      self.bottom = coord
-    else
-      self.right = coord
-    end
-  end
-
-  #ok wtf are you doing, Jeremy?
   # some PDFs (garment factory audits, precise link TK) make tables by drawing lines that
   # very nearly intersect each other, but not quite. E.g. a horizontal line spans the table at a Y val of 100
   # and each vertical line (i.e. column separating ruling line) starts at 101 or 102.
@@ -84,70 +18,12 @@ class Ruling
   PERPENDICULAR_PIXEL_EXPAND_AMOUNT = 2
   COLINEAR_OR_PARALLEL_PIXEL_EXPAND_AMOUNT = 1
 
-  # if the lines we're comparing are colinear or parallel, we expand them by a only 1 pixel,
-  # because the expansions are additive
-  # (e.g. two vertical lines, at x = 100, with one having y2 of 98 and the other having y1 of 102 would
-  # erroneously be said to nearlyIntersect if they were each expanded by 2 (since they'd both terminate at 100).
-  # The COLINEAR_OR_PARALLEL_PIXEL_EXPAND_AMOUNT is only 1 so the total expansion is 2.
-  # A total expansion amount of 2 is empirically verified to work sometimes. It's not a magic number from any
-  # source other than a little bit of experience.)
-
-  def nearlyIntersects?(another)
-    if self.intersectsLine(another)
-      true
-    elsif self.perpendicular_to?(another)
-      self.expand(PERPENDICULAR_PIXEL_EXPAND_AMOUNT).intersectsLine(another.expand(PERPENDICULAR_PIXEL_EXPAND_AMOUNT))
-    else
-      self.expand(COLINEAR_OR_PARALLEL_PIXEL_EXPAND_AMOUNT).intersectsLine(another.expand(COLINEAR_OR_PARALLEL_PIXEL_EXPAND_AMOUNT))
-    end
-  end
-
-  ##
-  # intersect this Ruling with a java.awt.geom.Rectangle2D
-  def intersect(area)
-    i = self.getBounds2D.createIntersection(area)
-    self.java_send :setLine, [Java::float, Java::float, Java::float, Java::float,], i.getX, i.getY, i.getX + i.getWidth, i.getY + i.getHeight
-    self
-  end
-
-  def expand(amt)
-    raise NoMethodError, "Oblique line #{self.inspect} has no #expand method." if oblique?
-    r = Ruling.new(self.top, self.left, self.width, self.height)
-    r.start = r.start - amt
-    r.end = r.end + amt
-    r
-  end
-
   def to_json(arg)
     [left, top, right, bottom].to_json
   end
 
   def ==(other)
     return self.getX1 == other.getX1 && self.getY1 == other.getY1 && self.getX2 == other.getX2 && self.getY2 == other.getY2
-  end
-
-  ##
-  # calculate the intersection point between +self+ and other Ruling
-  def intersection_point(other)
-    # algo taken from http://mathworld.wolfram.com/Line-LineIntersection.html
-
-    #self and other should always be perpendicular, since one should be horizontal and one should be vertical
-    self_l  = self.expand(PERPENDICULAR_PIXEL_EXPAND_AMOUNT)
-    other_l = other.expand(PERPENDICULAR_PIXEL_EXPAND_AMOUNT)
-
-    return nil if !self_l.intersectsLine(other_l)
-
-    horizontal, vertical = if self_l.horizontal? && other_l.vertical?
-                             [self_l, other]
-                           elsif self_l.vertical? && other_l.horizontal?
-                             [other_l, self_l]
-                           else
-                             raise ArgumentError, "must be orthogonal, horizontal and vertical"
-                           end
-
-
-    java.awt.geom.Point2D::Float.new(vertical.getX1, horizontal.getY1)
-
   end
 
   class HSegmentComparator
@@ -204,43 +80,6 @@ class Ruling
       }
   end
 
-  ##
-  # crop an enumerable of +Ruling+ to an +area+
-  def self.crop_rulings_to_area(rulings, area)
-    rulings.reduce([]) do |memo, r|
-      if r.intersects(area)
-        memo << r.clone.intersect(area)
-      end
-      memo
-    end
-  end
-
-  def self.collapse_oriented_rulings(lines)
-    # lines must all be of one orientation (i.e. horizontal, vertical)
-
-    if lines.empty?
-      return []
-    end
-
-    lines.sort! {|a, b| a.position != b.position ? a.position <=> b.position : a.start <=> b.start }
-
-    lines = lines.inject([lines.shift]) do |memo, next_line|
-      last = memo.last
-
-      # if current line colinear with next, and are "close enough": expand current line
-      if next_line.position == last.position && last.nearlyIntersects?(next_line)
-        memo.last.start = next_line.start < last.start ? next_line.start : last.start
-        memo.last.end = next_line.end < last.end ? last.end : next_line.end
-        memo
-      # if next line has no length, ignore it
-      elsif next_line.length == 0
-        memo
-      # otherwise, add it to the returned collection
-      else
-        memo << next_line
-      end
-    end
-  end
 end
 
 module Tabula
