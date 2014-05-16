efa60e368213680b857652d70247e94071e8d871
Manuel Aristarán <jazzido@jazzido.com>
2014-05-16 14:44:29 -0500

    commiting for jeremy

--- ext/tabula/src/main/java/org/nerdpower/tabula/extractors/SpreadsheetExtractionAlgorithm.java
+++ ext/tabula/src/main/java/org/nerdpower/tabula/extractors/SpreadsheetExtractionAlgorithm.java
@@ -4,11 +4,15 @@ import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
-
+import java.util.Set;
 import org.nerdpower.tabula.Cell;
 import org.nerdpower.tabula.Page;
+import org.nerdpower.tabula.Rectangle;
 import org.nerdpower.tabula.Ruling;
 import org.nerdpower.tabula.Table;
 
@@ -28,6 +32,26 @@ public class SpreadsheetExtractionAlgorithm implements ExtractionAlgorithm {
                 rv = 1;
             }
             else if (arg0.getX() < arg1.getX()) {
+                rv = -1;
+            }
+            return rv;
+        }
+    };
+    
+    private static final Comparator<Point2D> X_FIRST_POINT_COMPARATOR = new Comparator<Point2D>() {
+        @Override
+        public int compare(Point2D arg0, Point2D arg1) {
+            int rv = 0;
+            if (arg0.getX() > arg1.getX()) {
+                rv = 1;
+            }
+            else if (arg0.getX() < arg1.getX()) {
+                rv = -1;
+            }
+            else if (arg0.getY() > arg1.getY()) {
+                rv = 1;
+            }
+            else if (arg0.getY() < arg1.getY()) {
                 rv = -1;
             }
             return rv;
@@ -62,7 +86,6 @@ public class SpreadsheetExtractionAlgorithm implements ExtractionAlgorithm {
 
                 
             for (Point2D p: intersectionPointsList.subList(i, intersectionPointsList.size())) {
-//                System.out.println("here");
                 if (p.getX() == topLeft.getX() && p.getY() > topLeft.getY()) {
                     xPoints.add(p);
                 }
@@ -87,21 +110,148 @@ public class SpreadsheetExtractionAlgorithm implements ExtractionAlgorithm {
                         if (btmRightHV[0].colinear(xPoint) && btmRightHV[1].colinear(yPoint)) {
                             cellsFound.add(new Cell(topLeft, btmRight));
                         }
-//                        System.out.println("breaking");
                         doBreak = true;
                         break outer;
                     }
                 }
             }
-            // if (doBreak) { continue; }
         }
         return cellsFound;
+    }
+    
+    public List<Rectangle> findSpreadsheetsFromCells(List<Rectangle> cells) {
+        // via: http://stackoverflow.com/questions/13746284/merging-multiple-adjacent-rectangles-into-one-polygon
+        List<Rectangle> rectangles = new ArrayList<Rectangle>();
+        Set<Point2D> points = new HashSet<Point2D>();
+        Map<Point2D, Point2D> edgesH = new HashMap<Point2D, Point2D>();
+        Map<Point2D, Point2D> edgesV = new HashMap<Point2D, Point2D>();
+        int i = 0;
         
+        Collections.sort(cells);
         
+        for (Rectangle cell: cells) {
+            for(Point2D pt: cell.getPoints()) {
+                if (points.contains(pt)) {
+                    points.remove(pt);
+                }
+                else {
+                    points.add(pt);
+                }
+            }
+        }
+        
+        // X first sort
+        List<Point2D> pointsSortX = new ArrayList<Point2D>(points);
+        Collections.sort(pointsSortX, X_FIRST_POINT_COMPARATOR);
+        // Y first sort
+        List<Point2D> pointsSortY = new ArrayList<Point2D>(points);
+        Collections.sort(pointsSortY, POINT_COMPARATOR);
+        
+        while (i < points.size()) {
+            float currY = (float) pointsSortY.get(i).getY();
+            while (i < points.size() && pointsSortY.get(i).getY() == currY) {
+                edgesH.put(pointsSortY.get(i), pointsSortY.get(i+1));
+                edgesH.put(pointsSortY.get(i+1), pointsSortY.get(i));
+                i += 2;
+            }
+        }
+        
+        i = 0;
+        while (i < points.size()) {
+            float currX = (float) pointsSortX.get(i).getX();
+            while (i < points.size() && pointsSortX.get(i).getX() == currX) {
+                edgesV.put(pointsSortX.get(i), pointsSortX.get(i+1));
+                edgesV.put(pointsSortX.get(i+1), pointsSortX.get(i));
+                i += 2;
+            }
+        }
+        
+        // Get all the polygons
+        List<Polygon> polygons = new ArrayList<Polygon>();
+        Polygon polygon = new Polygon();
+        Point2D nextVertex;
+        while (!edgesH.isEmpty()) {
+            Point2D first = edgesH.keySet().iterator().next();
+            polygon.add(new PolygonVertex(first, Direction.HORIZONTAL));
+            edgesH.remove(first);
+            
+            while (true) {
+                PolygonVertex curr = polygon.get(polygon.size() - 1);
+                PolygonVertex lastAddedVertex;
+                if (curr.direction == Direction.HORIZONTAL) {
+                    nextVertex = edgesV.get(curr.point);
+                    edgesV.remove(curr.point);
+                    lastAddedVertex = new PolygonVertex(nextVertex, Direction.VERTICAL); 
+                    polygon.add(lastAddedVertex);
+                }
+                else {
+                    nextVertex = edgesH.get(curr.point);
+                    edgesH.remove(curr.point);
+                    lastAddedVertex = new PolygonVertex(nextVertex, Direction.HORIZONTAL);
+                    polygon.add(lastAddedVertex);
+                }
+                
+                if (lastAddedVertex.equals(polygon.get(0))) {
+                    // closed polygon
+                    polygon.remove(polygon.size() - 1);
+                    break;
+                }
+            }
+            for (PolygonVertex vertex: polygon) {
+                if (edgesH.containsKey(vertex.point)) {
+                    edgesH.remove(vertex.point);
+                }
+                if (edgesV.containsKey(vertex.point)) {
+                    edgesV.remove(vertex.point);
+                }
+            }
+            polygons.add(polygon);
+        }
+        
+        // calculate grid-aligned minimum area rectangles for each found polygon
+        for(Polygon poly: polygons) {
+            float top = java.lang.Float.MAX_VALUE;
+            float left = java.lang.Float.MAX_VALUE;
+            float bottom = java.lang.Float.MIN_VALUE;
+            float right = java.lang.Float.MIN_VALUE;
+            for (PolygonVertex pt: poly) {
+                top = (float) Math.min(top, pt.point.getY());
+                left = (float) Math.min(left, pt.point.getX());
+                bottom = (float) Math.max(bottom, pt.point.getY());
+                right = (float) Math.max(right, pt.point.getX());
+            }
+            rectangles.add(new Rectangle(top, left, right - left, bottom - top));
+        }
+        
+        return rectangles;
     }
     
     public String toString() {
         return "spreadsheet";
     }
+    
+    private enum Direction {
+        HORIZONTAL,
+        VERTICAL
+    }
+    
+    private class PolygonVertex {
+        Point2D point;
+        Direction direction;
+        
+        public PolygonVertex(Point2D point, Direction direction) {
+            this.direction = direction;
+            this.point = point;
+        }
+        
+        public boolean equals(PolygonVertex other) {
+            return this.point.equals(other.point) && this.direction == other.direction;
+        }
+    }
+    
+    @SuppressWarnings("serial")
+    private class Polygon extends ArrayList<PolygonVertex> {
+    }
+    
 
 }
