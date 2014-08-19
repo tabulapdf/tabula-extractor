acd804625ec7eec1f3d8eed5da374ae9cfe26122
Manuel Aristarán <jazzido@jazzido.com>
2014-08-19 18:08:52 -0300

    formatting

--- test/tests.rb
+++ test/tests.rb
@@ -206,7 +206,23 @@ class TestExtractor < Minitest::Test
 
     #puts table.rows.to_a.inspect
 
-    expected = [["AANONSEN, DEBORAH, A", "", "STATEN ISLAND, NY", "MEALS", "$85.00"], ["TOTAL", "", "", "", "$85.00"], ["AARON, CAREN, T", "", "RICHMOND, VA", "EDUCATIONAL ITEMS", "$78.80"], ["AARON, CAREN, T", "", "RICHMOND, VA", "MEALS", "$392.45"], ["TOTAL", "", "", "", "$471.25"], ["AARON, JOHN", "", "CLARKSVILLE, TN", "MEALS", "$20.39"], ["TOTAL", "", "", "", "$20.39"], ["AARON, JOSHUA, N", "", "WEST GROVE, PA", "MEALS", "$310.33"], ["", "REGIONAL PULMONARY & SLEEP"], ["AARON, JOSHUA, N", "", "WEST GROVE, PA", "SPEAKING FEES", "$4,700.00"], ["", "MEDICINE"], ["TOTAL", "", "", "", "$5,010.33"], ["AARON, MAUREEN, M", "", "MARTINSVILLE, VA", "MEALS", "$193.67"], ["TOTAL", "", "", "", "$193.67"], ["AARON, MICHAEL, L", "", "WEST ISLIP, NY", "MEALS", "$19.50"], ["TOTAL", "", "", "", "$19.50"], ["AARON, MICHAEL, R", "", "BROOKLYN, NY", "MEALS", "$65.92"]]
+    expected = [["AANONSEN, DEBORAH, A", "", "STATEN ISLAND, NY", "MEALS", "$85.00"],
+                ["TOTAL", "", "", "", "$85.00"],
+                ["AARON, CAREN, T", "", "RICHMOND, VA", "EDUCATIONAL ITEMS", "$78.80"],
+                ["AARON, CAREN, T", "", "RICHMOND, VA", "MEALS", "$392.45"],
+                ["TOTAL", "", "", "", "$471.25"],
+                ["AARON, JOHN", "", "CLARKSVILLE, TN", "MEALS", "$20.39"],
+                ["TOTAL", "", "", "", "$20.39"],
+                ["AARON, JOSHUA, N", "", "WEST GROVE, PA", "MEALS", "$310.33"],
+                ["", "REGIONAL PULMONARY & SLEEP"],
+                ["AARON, JOSHUA, N", "", "WEST GROVE, PA", "SPEAKING FEES", "$4,700.00"],
+                ["", "MEDICINE"],
+                ["TOTAL", "", "", "", "$5,010.33"],
+                ["AARON, MAUREEN, M", "", "MARTINSVILLE, VA", "MEALS", "$193.67"],
+                ["TOTAL", "", "", "", "$193.67"],
+                ["AARON, MICHAEL, L", "", "WEST ISLIP, NY", "MEALS", "$19.50"],
+                ["TOTAL", "", "", "", "$19.50"],
+                ["AARON, MICHAEL, R", "", "BROOKLYN, NY", "MEALS", "$65.92"]]
 
     expected.each_with_index do |row, i|
       row.each_with_index do |cell, j|
